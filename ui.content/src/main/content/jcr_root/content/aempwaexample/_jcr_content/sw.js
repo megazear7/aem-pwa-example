defd1a8d6f8dc5fbfc18bca827b2ed9e7bac70d9
alex.lockhart@icfolson.com <AlexLockhart@eaton.com>
2019-02-07 08:10:05 -0500

    Updated the service worker and updated the filter.xml to make sure to replace the service worker during a build.

--- ui.content/src/main/content/jcr_root/content/aempwaexample/_jcr_content/sw.js
+++ ui.content/src/main/content/jcr_root/content/aempwaexample/_jcr_content/sw.js
@@ -1,1 +1,16 @@
-console.log('hello from /content/aempwaexample/sw.js');
+var cacheName = 'aempwaexample-v1';
+var filesToCache = [
+  '/etc.clientlibs/aempwaexample/clientlibs/clientlib-base.css',
+  '/etc.clientlibs/aempwaexample/clientlibs/clientlib-base.js',
+  '/content/aempwaexample/en.html'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
